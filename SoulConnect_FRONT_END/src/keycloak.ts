bb657d8a82e32ac8fb95aecc93b6245d6241d786
karthikeyanbalanpkxlabs-alt <karthikeyanbalan.pkxlabs@gmail.com>
2026-07-06 23:03:40 +0530

    code updated

--- SoulConnect_FRONT_END/src/keycloak.ts
+++
@@ -1,9 +0,0 @@
-import Keycloak from "keycloak-js";
-
-const keycloak = new Keycloak({
-  realm: "soul_connect",
-  url: "http://localhost:4000/",
-  clientId: "soul_connect_c",
-});
-
-export default keycloak;
--- SoulConnect_FRONT_END/src/pages/PortalPage/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./PortalPage";
-export { default } from "./PortalPage";
