9b4836ede133a725c6c9d02bf36bc199a1382b92
karthikeyanbalanpkxlabs-alt <karthikeyanbalan.pkxlabs@gmail.com>
2026-07-06 23:35:22 +0530

    code updated

--- SoulConnect_BACK_END/src/apis_kit.ts
+++ SoulConnect_BACK_END/src/apis_kit.ts
@@ -14,34 +14,34 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:5174",
-  "http://127.0.0.1:5500",
-  "https://dev.soulconnect.com",
-  "https://soulconnect.com",
-];
-
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      // Allow requests with no origin (Postman, mobile apps, server-to-server)
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  }),
-);
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "http://localhost:5174",
+//   "http://127.0.0.1:5500",
+//   "https://dev.soulconnect.com",
+//   "https://soulconnect.com",
+// ];
 
 // app.use(
 //   cors({
-//     origin: "http://localhost:5173", // Frontend URL
+//     origin: (origin, callback) => {
+//       // Allow requests with no origin (Postman, mobile apps, server-to-server)
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
 //     credentials: true,
 //   }),
 // );
+
+app.use(
+  cors({
+    origin: "http://localhost:5174", // Frontend URL
+    credentials: true,
+  }),
+);
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
